7692b72d6f542e4d478cf080b85cee9ba44b7c03
Daniel O'Brien <daniel.o'brien@genomicsplc.com>
2021-12-25 18:08:26 +0000

    Add missing docs to Clap

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+```
+jointhedots 
+A simple git-based dotfile manager written entirely in Rust!
+
+USAGE:
+    jtd <SUBCOMMAND>
+
+OPTIONS:
+    -h, --help    Print help information
+
+SUBCOMMANDS:
+    help       Print this message or the help of the given subcommand(s)
+    install    Install a specified JTD repository
+    sync       Sync the currently installed JTD repository with the provided remote repo.
+    ```
+
 Grab the latest version [here](https://github.com/dob9601/jointhedots/releases/latest/download/jointhedots)!
--- src/cli.rs
+++ src/cli.rs
@@ -1,27 +1,28 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[clap(name = "jointhedots")]
-#[clap(bin_name = "jtd")]
+#[clap(name = "jointhedots", bin_name = "jtd", about)]
 pub enum JoinTheDots {
     Install(InstallSubcommandArgs),
     Sync(SyncSubcommandArgs)
 }
 
 #[derive(clap::Args)]
-#[clap(about, author, version)]
+#[clap(about = "Install a specified JTD repository", author, version)]
 pub struct InstallSubcommandArgs {
+    #[clap(help = "The location of the repository in the form USERNAME/REPONAME")]
     pub repository: String,
 
-    #[clap(default_value = "github")]
+    #[clap(default_value = "GitHub", help = "Whether to source the repo from GitHub or GitLab")]
     pub source: String
 }
 
 #[derive(clap::Args)]
-#[clap(about, author, version)]
+#[clap(about = "Sync the currently installed JTD repository with the provided remote repo.", author, version)]
 pub struct SyncSubcommandArgs {
+    #[clap(help = "The location of the repository in the form USERNAME/REPONAME")]
     pub repository: String,
 
-    #[clap(default_value = "github")]
+    #[clap(default_value = "GitHub", help = "Whether to source the repo from GitHub or GitLab")]
     pub source: String
 }
