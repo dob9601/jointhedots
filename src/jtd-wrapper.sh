4c6a823669f2ab4b3dd5b9a9fc7d8e4e6725ca0d
Daniel O'Brien <daniel.o'brien@genomicsplc.com>
2021-12-26 14:03:05 +0000

    Fix wrapper script

--- src/jtd-wrapper.sh
+++ src/jtd-wrapper.sh
@@ -1,4 +1,4 @@
-curl -L https://github.com/dob9601/jointhedots/releases/latest/download/jointhedots --output /tmp/jtdbin
+curl -L https://github.com/dob9601/jointhedots/releases/latest/download/jtd --output /tmp/jtdbin
 chmod +x /tmp/jtdbin
 /tmp/jtdbin $@
 
