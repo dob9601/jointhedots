42a6fda54e4f39adbf23288b8f7c6d68a2e37d80
Daniel O'Brien <dob9601@gmail.com>
2022-01-23 21:30:08 +0000

    Move git stuff into separate file

--- src/git.rs
+++ src/git.rs
@@ -1,9 +1,64 @@
-use std::error::Error;
+use std::{error::Error, path::Path, process::Command};
 
-use git2::Repository;
+use console::style;
+use dialoguer::{Input, Password};
+use git2::{Repository, Oid};
+use git2_credentials::{CredentialUI, CredentialHandler};
+
+use crate::utils::get_theme;
 
 
 pub fn get_head_hash(repo: &Repository) -> Result<String, Box<dyn Error>> {
     let commit = repo.head()?.target().ok_or("Could not get HEAD commit")?;
     Ok(commit.to_string())
+}
+
+pub struct CredentialUIDialoguer;
+
+impl CredentialUI for CredentialUIDialoguer {
+    fn ask_user_password(&self, username: &str) -> Result<(String, String), Box<dyn Error>> {
+        let theme = get_theme();
+        let user: String = Input::with_theme(&theme)
+            .default(username.to_owned())
+            .with_prompt("Username")
+            .interact()?;
+        let password: String = Password::with_theme(&theme)
+            .with_prompt("password (hidden)")
+            .allow_empty_password(true)
+            .interact()?;
+        Ok((user, password))
+    }
+
+    fn ask_ssh_passphrase(&self, passphrase_prompt: &str) -> Result<String, Box<dyn Error>> {
+        let passphrase: String = Password::with_theme(&get_theme())
+            .with_prompt(format!("{} (leave blank for no password): ", passphrase_prompt))
+            .allow_empty_password(true)
+            .interact()?;
+        Ok(passphrase)
+    }
+}
+pub fn clone_repo(url: &str, target_dir: &Path) -> Result<git2::Repository, Box<dyn Error>> {
+    // Clone the project.
+    let mut cb = git2::RemoteCallbacks::new();
+    let git_config = git2::Config::open_default().map_err(|err| format!("Could not open default git config: {}", err))?;
+    let mut ch = CredentialHandler::new_with_ui(git_config, Box::new(CredentialUIDialoguer {}));
+    cb.credentials(move |url, username, allowed| ch.try_next_credential(url, username, allowed));
+
+    // clone a repository
+    let mut fo = git2::FetchOptions::new();
+    fo.remote_callbacks(cb)
+        .download_tags(git2::AutotagOption::All)
+        .update_fetchhead(true);
+    let repo = git2::build::RepoBuilder::new()
+        .fetch_options(fo)
+        .clone(url, target_dir).map_err(|err| format!("Could not clone repo: {}", &err))?;
+
+    println!("{}", style("✔ Successfully cloned repository!").green());
+
+    Ok(repo)
+}
+
+pub fn is_in_past(repo: &Repository, commit_hash: &str) -> Result<bool, Box<dyn Error>> {
+    let head_commit = repo.head()?.target().ok_or("Could not get HEAD commit")?;
+    Ok(head_commit.to_string() == commit_hash)
 }
--- src/utils.rs
+++ src/utils.rs
@@ -46,52 +46,6 @@ pub fn get_repo_host_ssh_url(host: &str) -> Result<&str, Box<dyn Error>> {
         _ => Err("Provided host unknown".into()),
     }
 }
-pub struct CredentialUIDialoguer;
-
-impl CredentialUI for CredentialUIDialoguer {
-    fn ask_user_password(&self, username: &str) -> Result<(String, String), Box<dyn Error>> {
-        let theme = get_theme();
-        let user: String = Input::with_theme(&theme)
-            .default(username.to_owned())
-            .with_prompt("Username")
-            .interact()?;
-        let password: String = Password::with_theme(&theme)
-            .with_prompt("password (hidden)")
-            .allow_empty_password(true)
-            .interact()?;
-        Ok((user, password))
-    }
-
-    fn ask_ssh_passphrase(&self, passphrase_prompt: &str) -> Result<String, Box<dyn Error>> {
-        let passphrase: String = Password::with_theme(&get_theme())
-            .with_prompt(format!("{} (leave blank for no password): ", passphrase_prompt))
-            .allow_empty_password(true)
-            .interact()?;
-        Ok(passphrase)
-    }
-}
-pub fn clone_repo(url: &str, target_dir: &Path) -> Result<git2::Repository, Box<dyn Error>> {
-    // Clone the project.
-    let mut cb = git2::RemoteCallbacks::new();
-    let git_config = git2::Config::open_default().map_err(|err| format!("Could not open default git config: {}", err))?;
-    let mut ch = CredentialHandler::new_with_ui(git_config, Box::new(CredentialUIDialoguer {}));
-    cb.credentials(move |url, username, allowed| ch.try_next_credential(url, username, allowed));
-
-    // clone a repository
-    let mut fo = git2::FetchOptions::new();
-    fo.remote_callbacks(cb)
-        .download_tags(git2::AutotagOption::All)
-        .update_fetchhead(true);
-    let repo = git2::build::RepoBuilder::new()
-        .fetch_options(fo)
-        .clone(url, target_dir).map_err(|err| format!("Could not clone repo: {}", &err))?;
-
-    println!("{}", style("✔ Successfully cloned repository!").green());
-
-    Ok(repo)
-}
-// is this italic
-
 pub fn get_manifest(target_dir: &Path) -> Result<Manifest, Box<dyn Error>> {
     let mut path = target_dir.to_owned();
     path.push("jtd.yaml");
@@ -108,11 +62,4 @@ pub fn get_theme() -> impl Theme {
         values_style: Style::new().yellow().dim(),
         ..ColorfulTheme::default()
     }
-}
-
-pub fn is_in_past(commit_hash: &str) -> Result<bool, Box<dyn Error>> {
-    let command = Command::new("git")
-        .args(["merge-base", "--is-ancestor", commit_hash, "HEAD"])
-        .output()?;
-    Ok(command.status.success())
 }
